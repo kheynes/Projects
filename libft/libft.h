71d6242edc3da863856266c4635b9b16b585361f
Keziah Heynes <kheynes@c3r2s8.wethinkcode.co.za>
2019-05-21 14:44:27 +0200

    added prototypes to libft.h

---
+++ libft/libft.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kheynes <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/21 14:35:20 by kheynes           #+#    #+#             */
+/*   Updated: 2019/05/21 14:43:51 by kheynes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _LIBFT_H_
+#define _LIBFT_H_
+	void	ft_putchar(char c);
+	void 	ft_putstr(char const *s);
+	char	*ft_strcpy(char *dst, const char *src);
+	int	ft_strlen(const char *str);
+#endif /* _LIBFT_H_ */
