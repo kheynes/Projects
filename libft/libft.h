eb84298c0c1ffbce0d65d2e1755b7d7069e1b895
Keziah Heynes <kheynes@c3r2s8.wethinkcode.co.za>
2019-05-23 14:31:00 +0200

    added functions, modified a function

--- libft/libft.h
+++ libft/libft.h
@@ -6,20 +6,24 @@
 /*   By: kheynes <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 14:35:20 by kheynes           #+#    #+#             */
-/*   Updated: 2019/05/22 11:59:59 by kheynes          ###   ########.fr       */
+/*   Updated: 2019/05/23 10:49:19 by kheynes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef _LIBFT_H_
-#define _LIBFT_H_
-	void	ft_putchar(char c);
-	void 	ft_putstr(char const *s);
-	char	*ft_strcpy(char *dst, const char *src);
-	int	ft_strlen(const char *str);
-	int	ft_strcmp(char *s1, char *s2);
-	char	*ft_strdup(char *src);
-	int	ft_isalphar (int c);
-	int	ft_isdigit(int c);
-	int	ft_isascii(int c);
-	int	ft_isprint(int c);
-#endif /* _LIBFT_H_ */
+#ifndef __LIBFT_H__
+#define __LIBFT_H__
+
+void	ft_putchar(char c);
+void	ft_putstr(char const *s);
+char	*ft_strcpy(char *dst, const char *src);
+int	ft_strlen(const char *str);
+int	ft_strcmp(char *s1, char *s2);
+char	*ft_strdup(char *src);
+int	ft_isalpha (int c);
+int	ft_isdigit(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+int	ft_isalnum(int c);
+int	ft_toupper(int c);
+int	ft_tolower(int c);
+#endif
