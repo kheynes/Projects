34820507298100753784d16bbe05746f51848074
Keziah Heynes <kheynes@c3r2s8.wethinkcode.co.za>
2019-05-24 13:51:36 +0200

    fixed header file norme errors

--- libft/libft.h
+++ libft/libft.h
@@ -5,27 +5,30 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kheynes <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/21 14:35:20 by kheynes           #+#    #+#             */
-/*   Updated: 2019/05/23 10:49:19 by kheynes          ###   ########.fr       */
+/*   Created: 2019/05/24 11:18:51 by kheynes           #+#    #+#             */
+/*   Updated: 2019/05/24 11:18:57 by kheynes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef __LIBFT_H__
-#define __LIBFT_H__
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <string.h>
+# include <unistd.h>
+# include <stdlib.h>
 
 void	ft_putchar(char c);
 void	ft_putstr(char const *s);
 char	*ft_strcpy(char *dst, const char *src);
-int	ft_strlen(const char *str);
-int	ft_strcmp(char *s1, char *s2);
+int		ft_strlen(const char *str);
+int		ft_strcmp(char *s1, char *s2);
 char	*ft_strdup(char *src);
-int	ft_isalpha (int c);
-int	ft_isdigit(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-int	ft_isalnum(int c);
-int	ft_toupper(int c);
-int	ft_tolower(int c);
-int	ft_strncmp(char *s1, char *s2, size_t n);
+int		ft_isalpha (int c);
+int		ft_isdigit(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_isalnum(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_strncmp(char *s1, char *s2, size_t n);
 char	*ft_strncpy(char *dst, char *src, size_t n);
 #endif
