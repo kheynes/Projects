148fa11c0ccb043692516c6932de44527b2014b1
Keziah Heynes <kheynes@c3r2s8.wethinkcode.co.za>
2019-06-15 11:53:42 +0200

    Normed

--- libft/ft_lstnew.c
+++ libft/ft_lstnew.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kheynes <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/15 11:36:28 by kheynes           #+#    #+#             */
+/*   Updated: 2019/06/15 11:37:01 by kheynes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 t_list	*ft_lstnew(void const *content, size_t content_size)
@@ -5,7 +17,7 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 	t_list	*new;
 
 	new = (t_list*)malloc(sizeof(t_list));
-	if (new == NULL )
+	if (new == NULL)
 		return (NULL);
 	if (content == NULL)
 	{
--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kheynes <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/15 11:34:45 by kheynes           #+#    #+#             */
+/*   Updated: 2019/06/15 11:35:52 by kheynes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void	ft_putnbr_fd(int n, int fd)
@@ -7,13 +19,13 @@ void	ft_putnbr_fd(int n, int fd)
 		ft_putchar_fd('-', fd);
 		if (n <= -10)
 			ft_putnbr_fd(n / -10, fd);
-		ft_putchar_fd(-(n % 10) +'0', fd);
+		ft_putchar_fd(-(n % 10) + '0', fd);
 	}
 	else if (n >= 10)
 	{
 		ft_putnbr_fd(n / 10, fd);
 		ft_putchar_fd((n % 10) + '0', fd);
 	}
-	else 
-		ft_putchar_fd(n +'0', fd);
+	else
+		ft_putchar_fd(n + '0', fd);
 }
