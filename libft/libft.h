0d6a85cc751289dd3b3a65e0c0cf266c33e5f96d
Keziah Heynes <heyneskeziah@gmail.com>
2019-09-05 14:00:49 +0200

    Added get_next_line

--- libft/libft.h
+++ libft/libft.h
@@ -6,15 +6,18 @@
 /*   By: kheynes <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 11:18:51 by kheynes           #+#    #+#             */
-/*   Updated: 2019/07/02 10:17:21 by kheynes          ###   ########.fr       */
+/*   Updated: 2019/09/05 13:56:48 by kheynes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+# define BUFF_SIZE 1
+# define MAX_FD 255
 # include <string.h>
 # include <unistd.h>
 # include <stdlib.h>
+# include <fcntl.h>
 
 typedef struct	s_list
 {
@@ -82,4 +85,5 @@ void			ft_lstdel(t_list **alst, void (*del)(void*, size_t));
 void			ft_lstadd(t_list **alst, t_list *new);
 void			ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+int				get_next_line(const int fd, char **line);
 #endif
